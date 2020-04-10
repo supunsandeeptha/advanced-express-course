3ea4e6f8dbd4ab136fe5786f840e5ceffd623449
supunsandeeptha <sandeeptha.supun@gmail.com>
2020-04-10 18:41:07 +1000

    mounting the routes with server js

--- server.js
+++ server.js
@@ -2,21 +2,20 @@ const express = require('express');
 // load static content
 const path = require('path');
 
+const route = require('./routes/routes.js');
+
 const app = express();
 const port = 3000;
 // ejs template engine
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './views'));
+
 // using the static middleware to link the css styles and images
 app.use(express.static(path.join(__dirname, './static')));
 // serving index.html using path module
-app.get('/', (request, response) => {
-  response.render('pages/index', { pageTitle: 'Welcome' });
-});
-// serving speakers.html to link the css stypes and images
-app.get('/speakers', (request, response) => {
-  response.sendFile(path.join(__dirname, './static/speakers.html'));
-});
+
+// using the route middleware
+app.use('/', route());
 
 app.listen(3000, (err) => {
   if (err) {
