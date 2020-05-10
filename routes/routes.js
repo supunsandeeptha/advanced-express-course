285219b4cdd1719e299ccee7ceb70008f9d811a0
supunsandeeptha <sandeeptha.supun@gmail.com>
2020-05-10 14:43:18 +1000

    adding subroutes to the main router

--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,16 @@
-const express = require('express');
+const express = require("express");
+
+const speakersRoute = require("./speakers");
+const feedbackRoute = require("./feedback");
 
 const router = express.Router();
 
 module.exports = () => {
-  router.get('/', (request, response) => {
-    response.render('pages/index', { pageTitle: 'Welcome' });
+  router.get("/", (request, response) => {
+    response.render("pages/index", { pageTitle: "Welcome" });
   });
-
+  // other routes speakers and feedback
+  router.use("/speakers", speakersRoute());
+  router.use("/feedback", feedbackRoute());
   return router;
 };
